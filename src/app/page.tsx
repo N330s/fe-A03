991857f78304b1a1692d98405ca508c8af036c7c
n330s <wiritpol.poonnak@gmail.com>
2025-03-12 19:51:44 +0700

    fix name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Banner from "@/components/Bannner";
+import Banner from "@/components/Banner";
 import Card from "@/components/Card";
 
 export default function Home() {
   return (
-      <div style={{ margin: 0, padding: 0 }}>
+      <div>
       <Banner />
-      <div style={{ padding: '0 1rem' }}> 
-        <Card />
-      </div>
+      <Card />
     </div>
   );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,9 +3,9 @@ import styles from './banner.module.css';
 const Banner = () => {
   return (
     <div className={styles.banner}>
-      <img src="/banner-image.jpg" alt="Event Venue" className={styles.bannerImage} />
+      <img src ="/banner-image.jpg" alt="venue" className={styles.bannerImage} />
       <div className={styles.bannerText}>
-        <h1>Where every event finds its venue</h1>
+        <h1>where every event finds its venue</h1>
         <p>Finding the perfect venue has never been easier. Whether it’s a wedding, corporate event, or private party, we connect people to the perfect place.</p>
       </div>
     </div>
